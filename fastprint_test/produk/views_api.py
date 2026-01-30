236383234a5d2bbad8cdd942f5aeb2068f09fa47
Kennethcp <kenneth.chr2003@mail.ugm.ac.id>
2026-01-30 11:10:47 +0700

    add feature

--- fastprint_test/produk/views_api.py
+++ fastprint_test/produk/views_api.py
@@ -1,7 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Produk
 from .serializers import ProdukSerializer
 
 class ProdukViewSet(viewsets.ModelViewSet):
     queryset = Produk.objects.all()
     serializer_class = ProdukSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        confirm = request.data.get("confirm", False)
+
+        if not confirm:
+            return Response(
+                {"detail": "Konfirmasi diperlukan untuk menghapus data. Tambahkan 'confirm': true."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"detail": "Produk berhasil dihapus."}, status=status.HTTP_204_NO_CONTENT)
